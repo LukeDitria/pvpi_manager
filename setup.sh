189aeaa8d51f1e18f95350bd6b4387803181fc64
Luke Ditria <mrsirlrd@gmail.com>
2026-01-14 12:47:36 +1100

    pvpi test script

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ deactivate
 chmod +x pvpi_run.sh
 chmod +x uart_server.sh
 chmod +x set_pvpi_time.sh
+chmod +x pvpi_test.sh
 
 echo "Configuring systemd services..."
 sudo sed -e "s|TARGET_USER|$USER|g" -e "s|TARGET_DIR|$PWD|g" pvpi_manager.service | sudo tee /etc/systemd/system/pvpi_manager.service > /dev/null
